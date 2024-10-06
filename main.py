b6420efa10a0fb941fdfdabf74be013223e7f43d
lizka18 <elizavetaazaretova@gmail.com>
2024-10-06 17:03:02 +0300

    apdate main

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from new_funk import *
+from defferenc import *
 
-a = int(input())
-b = int(input())
 
-print(f'{a}+{b}={plus(a,b)}')
+def main():
+
+    print('введите первое число')
+    a = int(input())
+    print('введите второе число')
+    b = int(input())
+    print('выберите операцию. 1- сложение, 2 - разность')
+    op = int(input())
+
+    if op == 1:
+        print(f'{a}+{b}={plus(a, b)}')
+    elif op==2:
+        print(f'{a}-{b}={differ(a, b)}')
+    else:
+        print(f'Такой операции нет')
+
+if __name__ == "__main__":
+    while True:
+        main()
